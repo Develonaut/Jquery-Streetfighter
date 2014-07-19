077c5cee64c66243b635d5bbd8d1adee3b963bb5
Ryan McHenry <ryanmmchenry@gmail.com>
2014-07-18 23:14:03 -0400

    Added functionality for Hadouken

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
   
+var $ready = $('.ryu-ready');
+var $still = $('.ryu-still');
+var $ryuThrow = $(".ryu-throwing");
+var $hadouken = $(".hadouken");
+
+ function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+  }
+
 
   $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
+    $still.hide();
+    $ready.show();
   })
   .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
+    $ready.hide();
+    $still.show();
+  })
+  .mousedown(function() {
+  	playHadouken();
+  	$ready.hide();
+  	$ryuThrow.show();
+  	$hadouken.finish().show().animate(
+  		{"left":"300px"}, 300, 
+  		function() { 
+  			$(this).hide(); 
+  			$(this).css("left", "-212px"); 
+  		});
+  })
+  .mouseup(function() {
+  	$ryuThrow.hide();
+  	$ready.show();
+  	$hadouken.hide();
   });
+
+
