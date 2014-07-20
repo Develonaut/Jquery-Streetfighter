3437c966a870179bf48ac5c665bdf77e6ef0e414
Ryan McHenry <ryanmmchenry@gmail.com>
2014-07-20 11:38:38 -0400

    Added in script for Intro Animation

--- js/min/app-ck.js
+++ js/min/app-ck.js
@@ -5,29 +5,81 @@ var $ryuThrow = $(".ryu-throwing");
 var $hadouken = $(".hadouken");
 var $cool = $(".ryu-cool");
 var $ryu_actions = $(".ryu-action");
+var $streetLogo = $("#streetLogo");
+var $madeWith = $("#madeWith");
+var $jqueryLogo = $("#jqueryLogo");
 
-	 
+
+function playIntroSound() {
+	$("#intro_sound")[0].volume = 0.5;
+	$("#intro_sound")[0].play();
+}
+
+
+// Play start sound on
 function playStartSound() {
 	$("#start_sound")[0].volume = 1;
 	$("#start_sound")[0].play();
-}
+} 
 
+// Play Hadouken sound
  function playHadouken() {
   $("#hadouken-sound")[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $("#hadouken-sound")[0].play();
   }
 
+// Play groove sound
 function playGroove () {
     $('#groove')[0].pause();
     $("#groove")[0].volume = 0.5;
     $('#groove')[0].play();
 }
 
+
+//********** OVERLAY START SCREEN *************//
+
+$("#overlay").on("click", function() {
+ 	$(this).hide();
+ 	playStartSound();
+ 	playIntroSound();
+	$(".instructions").hide();
+	$("#instructions_toggle").hide();
+	$streetLogo.fadeIn(2500, function() {
+		$streetLogo.fadeOut(1000);
+		$madeWith.fadeIn(2500, function() {
+			$madeWith.fadeOut(1000);
+			$jqueryLogo.fadeIn(2500, function() {
+				$jqueryLogo.fadeOut(1000);
+				$(".instructions").fadeIn(1000);
+				$("#instructions_toggle").fadeIn(1000);
+			});
+		});
+	});
+});
+
+
+
+
+
+
+//********** INSTRUCTIONS TOGGLE BUTTON *************//
+
+$("#instructions_toggle").on("click", function() {
+	$(".instructions").toggle();
+});
+
+
+//********** BEGGINING OF RYU ACTIONS *************//
+
+  // When the mouse enters the ryu container
   $('.ryu').mouseenter(function() {
+    // Hide all of Ryu's actions
     $ryu_actions.hide();
+    // Show Ryu's ready stance
     $ready.show();
   })
+  // When the mouse leaves the Ryu container
   .mouseleave(function() {
     $ryu_actions.hide();
     $still.show();
@@ -56,6 +108,8 @@ function playGroove () {
   				$ryuThrow.hide();
   				$ready.hide();
   				$still.hide();
+  				$("#intro_sound")[0].pause();
+  				$("#intro_sound")[0].load();
   				playGroove();
   			}
   });
@@ -63,8 +117,8 @@ function playGroove () {
 
     $(document).on("keyup", function(event) {
   		if (event.keyCode === 88) {
-  			$ready.show(); 
-			$still.hide(); 
+  			$ready.hide(); 
+			$still.show(); 
 			$ryuThrow.hide();
 			$hadouken.hide();
 			$cool.hide();
@@ -73,17 +127,4 @@ function playGroove () {
   		}
   });
 
-
-
-	$("#overlay").on("click", function() {
-	 	$(this).hide();
-	 	playStartSound();
-	 	$(".instructions").show();
-	 	$("#instructions_toggle").show();
-	 });
-
-
-	$("#instructions_toggle").on("click", function() {
-		$(".instructions").toggle();
-	});
 
